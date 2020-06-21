5f3bbc0bb560884ab959bd5146d4e8b5a9531943
chima <chimaataman@gmail.com>
2020-06-21 15:33:53 +0100

    refine code

--- chatbot.py
+++ chatbot.py
@@ -1,28 +1,33 @@
+import sys
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
 revolve_bot = ChatBot(name='RevolveBot', read_only=True,
                       logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
-                      
-small_talk = [
-               'Hi',
-               'Hello',
-               'How are you?'
-               'How are you doing?'
-               'What’s up?'
-               'Good day',
-               'Tell me something',
-               'I’ll do that now',
-               'Hello',
-               'Thank you',
-               'Goodbye',
-               'How can you help me?',
-               'Hi, my name is RevolveBot',
-               'Happy birthday!',
-               'I have a question',
-              ]
-small_talk_trainer = ListTrainer(revolve_bot)    
-for item in (small_talk,):
-    small_talk_trainer.train(item)
-    
+def train():                      
+    small_talk = [
+                   'Hi',
+                   'Hello',
+                   'How are you?'
+                   'How are you doing?'
+                   'What’s up?'
+                   'Good day',
+                   'Tell me something',
+                   'I’ll do that now',
+                   'Hello',
+                   'Thank you',
+                   'Goodbye',
+                   'How can you help me?',
+                   'Hi, my name is RevolveBot',
+                   'Happy birthday!',
+                   'I have a question',
+                  ]
+    small_talk_trainer = ListTrainer(revolve_bot)    
+    for item in (small_talk,):
+        small_talk_trainer.train(item)
+        
+if __name__ == '__main__':
+    command = sys.argv[1]
+    if command == 'train':
+        train()
               
